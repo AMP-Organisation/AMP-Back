72f798396fb732a3c6d941f5222acfcfbd1187fc
Unknow46 <nathan.sens@hotmail.fr>
2021-02-25 12:04:14 +0100

    :construction: testing linter

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 # This is a sample Python script.
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from fastapi import FastAPI
 from routers import user, information
 
