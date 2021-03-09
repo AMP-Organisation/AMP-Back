b689a49a702770e72b1590571c4c662511cf9818
Unknow46 <nathan.sens@hotmail.fr>
2021-03-09 18:09:01 +0100

    :construction: adding new route 'place'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from .core.settings import settings
+from .routers.place_route import place_router
 
 app = FastAPI()
+
+app.include_router(place_router, prefix=settings.API_V1_STR)
+
 
 @app.get("/")
 def read_root():
