4ef291df1a0ba8c35a3297ecf14bbf4a4cba3d10
Unknow46 <nathan.sens@hotmail.fr>
2021-03-23 20:58:40 +0100

    :bug: forgot to change SQLALCHEMY_DATABASE_URI -> DATABASE_URL

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,14 +1,15 @@
 import secrets
 from pydantic import BaseSettings
 
+
 class Settings(BaseSettings):
-    API_V1_STR: str
-    PROJECT_NAME: str 
+    API_V1_STR: str = ''
+    PROJECT_NAME: str = ''
     SECRET_KEY: str = secrets.token_urlsafe(32)
-    SQLALCHEMY_DATABASE_URI: str
+    DATABASE_URL: str = ''
+
     class Config:
         env_file = '.env'
-
 
 
 settings = Settings()
