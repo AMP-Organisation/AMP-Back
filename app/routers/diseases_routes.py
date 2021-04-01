040ebcf53acb9fd6f2ad4a906ec5b2085bf6b13c
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2021-04-01 15:48:05 +0200

    :consrtuction: remove some print

--- app/routers/diseases_routes.py
+++ app/routers/diseases_routes.py
@@ -58,35 +58,14 @@ def put_disease(body_disease: disease_schemas.moreDisease, dbSession: Session = 
     return updated_disease
     #return {"message":"in progress PUT"}
 
-# In this function I call the put function to update totally and no partially
+# for now, PATCH only work on descriptioon and name
+# And PARTIALLY : the ID stay the same, juste up the others data
 @diseases_router.patch("/")
 def patch_disease(body_disease: disease_schemas.diseasePatch, dbSession: Session = Depends(db_dependencies.get_db)):
     print("*in PATCH route for disease*")
     print(body_disease)
     patched = crudObjDisease.patch_a_disease(dbSession, body_disease, body_disease.id)
     return patched
-    # disease_in_base = crudObjDisease.get_one_disease(dbSession, body_disease.id)
-    # print("disease to Patch")
-    # print(disease_in_base)
-    # print(disease_in_base.id)
-    # print(disease_in_base.is_treatment)
-    # print(disease_in_base.danger_level)
-    # newName = body_disease.name if body_disease.name != None else disease_in_base.name_disease
-    # newDescription = body_disease.description if body_disease.description != None else disease_in_base.description
-    # # i create a full schema object from the data receive (to update) and data in db
-    # updated_disease = disease_schemas.moreDisease(
-    #     id = disease_in_base.id,
-    #     name = newName,
-    #     description = newDescription,
-    #     is_vaccine = disease_in_base.is_vaccine,
-    #     is_treatment = disease_in_base.is_treatment,
-    #     danger_level = disease_in_base.danger_level
-    # )
-    # print(updated_disease)
-    #updated_disease_final = crudObjDisease.update_disease(dbSession, updated_disease)
-    #return updated_disease_final
-    #return {"message":"in progress PATCH"}
-
 
 # on va vraiment faire un delete ? en tout cas seul les admins devrait pouvoir le faire non ?
 @diseases_router.delete("/")
