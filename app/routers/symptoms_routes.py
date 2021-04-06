526f4a08e64912124cc362466428ac2ba5b30150
Unknow46 <nathan.sens@hotmail.fr>
2021-04-06 16:40:45 +0200

    :construction: adding route in symptoms to get all symptoms

--- app/routers/symptoms_routes.py
+++ app/routers/symptoms_routes.py
@@ -3,6 +3,7 @@ from typing import List
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from ..core.db_dependencies import get_db
+from ..database.models.symtoms_model import Symptoms
 from ..schemas import symptoms_schema, disease_schemas
 from ..crud.crud_symptoms import symptoms
 
@@ -11,10 +12,15 @@ symptoms_router = APIRouter(
 )
 
 
-# user log in
+@symptoms_router.get('/getAll', response_model=List[symptoms_schema.SymptomsResponse])
+def all_symptoms(*, db: Session = Depends(get_db), skip: int = 0, limit: int = 100) -> List[Symptoms]:
+    return symptoms.get_multi(db, skip=skip, limit=limit)
+
+
+# user search for disease with those symptoms
 @symptoms_router.post('/getSymptoms', response_model=List[disease_schemas.responseDisease])
-def connection_user(*, db: Session = Depends(get_db), auth_in: symptoms_schema.GetSymptoms):
-    all_disease_filtered = symptoms.get_disease_filtered(db, auth_in.all_name)
+def disease_filtered_by_symptoms(*, db: Session = Depends(get_db), symptoms_in: symptoms_schema.GetSymptoms):
+    all_disease_filtered = symptoms.get_disease_filtered(db, symptoms_in.all_name)
 
     if not all_disease_filtered:
         raise HTTPException(
