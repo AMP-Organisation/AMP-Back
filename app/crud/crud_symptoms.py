eade186f3dbef69cc3c40f0eb4ab22b8b3ec3ca7
Unknow46 <nathan.sens@hotmail.fr>
2021-04-07 09:59:07 +0200

    :construction: refacto function disease filtered

--- app/crud/crud_symptoms.py
+++ app/crud/crud_symptoms.py
@@ -9,13 +9,15 @@ from typing import Optional, List
 
 class CRUDSymptoms(CRUDBase[Symptoms, CreateSymptoms, UpdateSymptoms]):
 
+    def get_id_by_name(self, db: Session, name: str) -> int:
+        result = db.query(self.model).filter_by(name=name).first()
+        return result.id
+
     def get_disease_filtered(self, db: Session, all_name: List[str]) -> List[disease]:
-        symptoms_id = []
-        for name in all_name:
-            current_symptoms_id = db.query(self.model).filter_by(name=name).first()
-            symptoms_id.append(current_symptoms_id.id)
+        symptoms_id = [self.get_id_by_name(db, name) for name in all_name]
         disease_filtered = db.query(disease).filter(disease.symptoms.contains(symptoms_id)).all()
         return disease_filtered
+
     pass
 
 
