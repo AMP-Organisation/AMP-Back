20a89725f4394b058ef924c15d64e56a6ff02168
Unknow46 <nathan.sens@hotmail.fr>
2021-05-03 08:27:25 +0200

    :construction: added route to update treatment

--- app/routers/treatment_routes.py
+++ app/routers/treatment_routes.py
@@ -4,8 +4,8 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from ..core.db_dependencies import get_db
-from ..schemas import treatment_schema, medicine_schema
 from ..crud.treatment_crud import treatment
+from ..schemas import treatment_schema, medicine_schema, message
 
 treatment_router = APIRouter(
     prefix='/treatment'
@@ -24,7 +24,28 @@ def show_treatment_related(*, db: Session = Depends(get_db), pillbox_in: treatme
     return all_treatment_related
 
 
+@treatment_router.post('/updateTreatment', response_model=message.Message)
+def show_treatment_related(*, db: Session = Depends(get_db), treatment_in: treatment_schema.TreatmentBase):
+    print(treatment_in)
+    current_treatment = treatment.get(db=db, model_id=treatment_in.id)
+
+    if not current_treatment:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="The treatment do not exist in the system.",
+        )
+
+    treatment.update(db=db, db_obj=current_treatment, obj_in=treatment_in)
+    return {"message": "Traitement mis à jour"}
+
+
+@treatment_router.get('/treatmentUser/{user_id}', response_model=List[treatment_schema.TreatmentResponse])
+def show_treatment_related(user_id: int, db: Session = Depends(get_db)):
+    user_treatment = treatment.user_treatment(db, user_id)
+    return user_treatment
+
+
 @treatment_router.post('/medicineRelated', response_model=List[medicine_schema.medicineResponse])
-def show_medicine_related(*, db:Session = Depends(get_db), pillbox_in: treatment_schema.GetTreatment):
+def show_medicine_related(*, db: Session = Depends(get_db), pillbox_in: treatment_schema.GetTreatment):
     all_medicine_related = treatment.related_medicine(db, pillbox_in.current_treatment)
     return all_medicine_related
