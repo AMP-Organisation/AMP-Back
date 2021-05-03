8cca07f522b48da5becf1fd2dc863c5ad671af8b
Unknow46 <nathan.sens@hotmail.fr>
2021-05-03 09:46:10 +0200

    :construction: added route to create a treatment

--- app/routers/treatment_routes.py
+++ app/routers/treatment_routes.py
@@ -25,8 +25,7 @@ def show_treatment_related(*, db: Session = Depends(get_db), pillbox_in: treatme
 
 
 @treatment_router.post('/updateTreatment', response_model=message.Message)
-def show_treatment_related(*, db: Session = Depends(get_db), treatment_in: treatment_schema.TreatmentBase):
-    print(treatment_in)
+def update_treatment(*, db: Session = Depends(get_db), treatment_in: treatment_schema.TreatmentBase):
     current_treatment = treatment.get(db=db, model_id=treatment_in.id)
 
     if not current_treatment:
@@ -35,17 +34,42 @@ def show_treatment_related(*, db: Session = Depends(get_db), treatment_in: treat
             detail="The treatment do not exist in the system.",
         )
 
+    medicine_in = treatment_in.medicine_id
+    update_medicine = False
+    for medicine in medicine_in:
+        if medicine not in current_treatment.medicine_id:
+            update_medicine = True
+
+    if not update_medicine:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Ce médicament fait déjà partie de cette liste de traitement.",
+        )
+
     treatment.update(db=db, db_obj=current_treatment, obj_in=treatment_in)
     return {"message": "Traitement mis à jour"}
 
 
+@treatment_router.post('/createTreatment', response_model=message.Message)
+def create_treatment(*, db: Session = Depends(get_db), treatment_in: treatment_schema.TreatmentBase):
+    current_treatment = treatment.verify_name(db=db, user_id=treatment_in.user_id, name=treatment_in.name)
+
+    if current_treatment:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Un traitement existe déjà avec ce nom",
+        )
+    treatment.create(db=db, obj_in=treatment_in)
+    return {"message": "Le traitement à bien été créé."}
+
+
 @treatment_router.get('/treatmentUser/{user_id}', response_model=List[treatment_schema.TreatmentResponse])
-def show_treatment_related(user_id: int, db: Session = Depends(get_db)):
+def get_user_treatments(user_id: int, db: Session = Depends(get_db)):
     user_treatment = treatment.user_treatment(db, user_id)
     return user_treatment
 
 
 @treatment_router.post('/medicineRelated', response_model=List[medicine_schema.medicineResponse])
-def show_medicine_related(*, db: Session = Depends(get_db), pillbox_in: treatment_schema.GetTreatment):
+def show_all_medicine_related(*, db: Session = Depends(get_db), pillbox_in: treatment_schema.GetTreatment):
     all_medicine_related = treatment.related_medicine(db, pillbox_in.current_treatment)
     return all_medicine_related
