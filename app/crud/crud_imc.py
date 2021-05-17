39460b48907a80aa95164d733ff9e02e3075a6f1
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2021-05-17 17:19:39 +0200

    :construction: créé d'autre route pour le suivi de l'imc
    ... dont une pour avoir les données sur une période donnée. J'ai fait d'auyre routes, certaines redondante, mais par simplicité, pour avoir les donnée sur une semain un mois un an.

--- app/crud/crud_imc.py
+++ app/crud/crud_imc.py
@@ -6,6 +6,7 @@ from ..crud.crud_base import CRUDBase
 from ..schemas.followup_schema import followup_imc
 
 from ..database.models import followup_model
+from datetime import date, datetime, timedelta
 
 # étant donné que pourrait avoir plusieurs suivi, est ce qu'on fait un ensemble 
 # (crud, schema et mode) pour chaque type de suivi ou un ensemble qui prend tout
@@ -15,8 +16,14 @@ class CRUD_IMC:
         return dbSession.query(followup_model.imc_suivi).limit(limit).all()
 
     def get_all_data_from_one_user(self, dbSession: Session, limit: int, id_user: int):
-        return dbSession.query(followup_model.imc_suivi).filter(followup_model.imc_suivi.user_id == id_user).limit(limit).all()
+        return dbSession.query(followup_model.imc_suivi).filter(followup_model.imc_suivi.user_id == id_user).order_by(followup_model.imc_suivi.date.desc()).limit(limit).all()
 
+    def get_data_period(self, dbSession: Session, id_user: int, nbDay: int):
+        timeD = timedelta(days=nbDay)
+        today = date.today()
+        antes = date.today() - timeD
+        last_week = dbSession.query(followup_model.imc_suivi).filter(followup_model.imc_suivi.user_id == id_user).filter(followup_model.imc_suivi.date.between(antes, today)).order_by(followup_model.imc_suivi.date.desc()).all()
+        return last_week
 
     def get_one_item(self, dbSession: Session, id_to_find: int):
         return dbSession.query(followup_model.imc_suivi).filter(followup_model.imc_suivi.id == id_to_find)
--- app/routers/followup_routes.py
+++ app/routers/followup_routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status, Response
 from sqlalchemy.orm import Session
 from ..database import session, base_class
 
@@ -16,6 +16,19 @@ follow_up_router = APIRouter(
 
 crud_obj_imc = crud_imc.CRUD_IMC()
 
+@follow_up_router.get("/imc/period")
+def get_on_period_data(response: Response, db_session: Session = Depends(db_dependencies.get_db), id_user: int = 0, duration: int = 7):
+    print("GET period")
+    print(id_user)
+    print(duration)
+    if id_user == 0:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return "missing id_user in query"
+    on_duration = crud_obj_imc.get_data_period(db_session, id_user, duration)
+    return on_duration
+
+
+
 # attention la j'ai précisé /imc ce qui donne comme route /followup/imc
 # comme ca on pourra avoir d'autre route followup/toto 
 @follow_up_router.get("/imc/{id_user}")
@@ -25,11 +38,41 @@ def get_all_data(id_user: int, db_session: Session = Depends(db_dependencies.get
     all_data = crud_obj_imc.get_all_data_from_one_user(db_session, limit, id_user)
     return all_data
 
+
 @follow_up_router.get("/imc")
 def get_all_data(db_session: Session = Depends(db_dependencies.get_db), limit: int = 365, id: int = 0):
-    print("GET ")
+    print("GET /imc")
     all_data = crud_obj_imc.get_all_data(db_session, limit)
     return all_data
+
+
+# j'ai créé des routes lastweek lastmonth et lastyear 
+# par simplicité, même si c'est redondant ^^
+# sur 7 jour pour une semaine
+@follow_up_router.get("/imc/lastweek/{id_user}")
+def get_last_week_data(id_user: int, db_session: Session = Depends(db_dependencies.get_db)):
+    print("GET last week")
+    print(id_user)
+    last_week_data = crud_obj_imc.get_data_period(db_session, id_user, 7)
+    return last_week_data
+
+
+# sur 365 jours
+@follow_up_router.get("/imc/lastyear/{id_user}")
+def get_last_year_data(id_user: int, db_session: Session = Depends(db_dependencies.get_db)):
+    print("GET last year")
+    print(id_user)
+    return crud_obj_imc.get_data_period(db_session, id_user, 365)
+
+
+#sur 31 jour pour un mois
+@follow_up_router.get("/imc/lastmonth/{id_user}")
+def get_last_month_data(id_user: int, db_session: Session = Depends(db_dependencies.get_db)):
+    print("GET last month")
+    print(id_user)
+    return crud_obj_imc.get_data_period(db_session, id_user, 31)
+
+
 
 @follow_up_router.post("/imc")
 def add_new_imc_data(body_followup_imc: followup_schema.followup_imc, \
@@ -40,3 +83,4 @@ def add_new_imc_data(body_followup_imc: followup_schema.followup_imc, \
     dbSession = db_session
     new_elem = crud_obj_imc.add_one_elem(body_followup_imc, dbSession)
     return new_elem
+
